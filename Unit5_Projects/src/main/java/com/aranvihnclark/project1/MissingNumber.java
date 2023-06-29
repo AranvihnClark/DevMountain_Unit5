4e94f3ef10f355c44ccd6dcc9628eaeff578ee4f
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-29 13:16:15 -0400

    Project 4 - Updated code/comments on project 1 for project 4 testing

--- Unit5_Projects/src/main/java/com/aranvihnclark/project1/MissingNumber.java
+++ Unit5_Projects/src/main/java/com/aranvihnclark/project1/MissingNumber.java
@@ -8,16 +8,20 @@ public class MissingNumber {
         // Missing "12".
         int[] array2 = { 9, 10, 1, 2, 7, 6, 11, 13, 5, 3, 8, 4 };
 
-        System.out.println(locateMissingNumber(array, array.length + 1));
-        System.out.println(locateMissingNumber(array2, array2.length + 1));
+        System.out.print("\nLocate via sorting: ");
+        System.out.print(missingNumberSort(array, array.length + 1));
+        System.out.println(missingNumberSort(array2, array2.length + 1));
+
+        System.out.print("\nLocate via adding: ");
+        System.out.print(missingNumberSum(array, array.length + 1));
+        System.out.println(missingNumberSum(array2, array2.length + 1));
     }
 
-    /*
     //*****************************************************
     //************** Step1 - Reduce Runtime ***************
     //*****************************************************
 
-    public static int locateMissingNumber(int[] arr, int maxNum) {
+    public static int missingNumberSort(int[] arr, int maxNum) {
 
         // Variable to hold the total that the array parameter should have if it was all added.
         // Since our for loop later won't add this for us, we can just have our total start out equaling "maxNum".
@@ -39,13 +43,12 @@ public class MissingNumber {
         // We return the value of the actual total minus the total of the array to get the missing number.
         return total - arrMaxValue;
     }
-    */
 
     //*****************************************************
     //************** Step2 - Reduce Memory ****************
     //*****************************************************
 
-    public static int locateMissingNumber(int[] arr, int maxNum) {
+    public static int missingNumberSum(int[] arr, int maxNum) {
         // The 'maxNum' parameter is honestly not needed as all we needed to do to get it is "arr.length".
         // As such, I will repurpose this variable where it will represent the missing number for this purpose.
 
