b9966e00734e77f2b6ca557fba147851b30d4586
Aranvihn Clark <aranvihn.clark@gmail.com>
2023-06-29 14:02:44 -0400

    Project 4 - Step 1 Complete - Project 1, Step 1 code and notes updated

--- Unit5_Projects/src/main/java/com/aranvihnclark/project1/MissingNumber.java
+++ Unit5_Projects/src/main/java/com/aranvihnclark/project1/MissingNumber.java
@@ -9,19 +9,19 @@ public class MissingNumber {
         int[] array2 = { 9, 10, 1, 2, 7, 6, 11, 13, 5, 3, 8, 4 };
 
         System.out.print("\nLocate via sorting: ");
-        System.out.print(missingNumberSort(array, array.length + 1));
-        System.out.println(missingNumberSort(array2, array2.length + 1));
+        System.out.print(findMissingNumberOne(array, array.length + 1));
+        System.out.println(findMissingNumberOne(array2, array2.length + 1));
 
         System.out.print("\nLocate via adding: ");
-        System.out.print(missingNumberSum(array, array.length + 1));
-        System.out.println(missingNumberSum(array2, array2.length + 1));
+        System.out.print(findMissingNumberTwo(array, array.length + 1));
+        System.out.println(findMissingNumberTwo(array2, array2.length + 1));
     }
 
     //*****************************************************
     //************** Step1 - Reduce Runtime ***************
     //*****************************************************
 
-    public static int missingNumberSort(int[] arr, int maxNum) {
+    public static int findMissingNumberOne(int[] arr, int maxNum) {
 
         // Variable to hold the total that the array parameter should have if it was all added.
         // Since our for loop later won't add this for us, we can just have our total start out equaling "maxNum".
@@ -48,9 +48,10 @@ public class MissingNumber {
     //************** Step2 - Reduce Memory ****************
     //*****************************************************
 
-    public static int missingNumberSum(int[] arr, int maxNum) {
+    public static int findMissingNumberTwo(int[] arr, int maxNum) {
         // The 'maxNum' parameter is honestly not needed as all we needed to do to get it is "arr.length".
         // As such, I will repurpose this variable where it will represent the missing number for this purpose.
+        // ** This was coded under the impression I didn't need to worry about an array not missing a value.
 
         // We iterate through each value in the array again.
         for (int i = 0; i < arr.length; i++) {
@@ -71,4 +72,26 @@ public class MissingNumber {
     //*****************************************************
 
     // It seems I already did so when handling steps 1 and 2...
+
+    //*****************************************************
+    //**************** Project4 - Testing *****************
+    //*****************************************************
+    public static int missingNumber(int[] numbers, int maxNum) {
+
+        // I create a variable just because it made it easier to understand what was going on, though I do feel it isn't really necessary.
+        int test = maxNum;
+
+        // Because we are trying to account for arrays that aren't missing any numbers, we need to check that first.
+        if (maxNum == numbers.length) {
+            return 0;
+        }
+
+        for (int i = 0; i < numbers.length; i++) {
+            test += i + 1;
+            test -= numbers[i];
+        }
+
+        // Then we return our test result because we know there is an issue.
+        return test;
+    }
 }
--- Unit5_Projects/src/test/java/MissingNumberTest.java
+++ Unit5_Projects/src/test/java/MissingNumberTest.java
@@ -4,24 +4,28 @@ import com.aranvihnclark.project1.MissingNumber;
 import org.junit.Test;
 
 public class MissingNumberTest {
-    private int[] missingMax = { 1, 2, 3, 4, 5, 6, 7, 8, 9 };
-    private int[] missingNone = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 };
-    private int[] missingSeven = { 1, 2, 3, 4, 5, 6, 8, 9, 10 };
+
+    // Just some random arrays for testing.
+    final private int[] missingNone = { 1, 2, 3, 4, 5, 6, 7, 8, 9, 10 }; // 0
+    final private int[] missingTen = { 1, 2, 3, 4, 5, 6, 7, 8, 9 }; // 10
+    final private int[] missingSomething = { 5, 1, 4, 2, 7, 10, 8, 9, 6 }; // 3
+
+    // I wasn't going to create this variable to hold our class, but I guess it does make sense in that it reduces the amount of typing needed.
     private MissingNumber mn = new MissingNumber();
 
     @Test
-    public void testMissingNumber_MissingMax() {
-        assertEquals(10, mn.missingNumber(missingMax, 10));
+    public void testMissingNumber_MissingTen() {
+        assertEquals(10, mn.missingNumber(missingTen, 10));
     }
 
     @Test
-    public void testMissingNumber_NoneMissing() {
+    public void testMissingNumber_MissingNone() {
         assertEquals(0, mn.missingNumber(missingNone, 10));
     }
 
     @Test
-    public void testMissingNumber_MissingSeven() {
-        assertEquals(7, mn.missingNumber(missingSeven, 10));
+    public void testMissingNumber_MissingSomething() {
+        assertEquals(3, mn.missingNumber(missingSomething, 10));
     }
 
 
